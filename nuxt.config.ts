288ad27ea1cdfc2f25a94aa0ad46197ad314a20b
Renzo <renzors@gmail.com>
2025-05-09 21:52:38 +0100

    public api base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE
     }
   },
 
